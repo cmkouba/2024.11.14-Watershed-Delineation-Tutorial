dcd9cb8b8a16731712103154363ef54a2906335a
cmkouba <40478341+cmkouba@users.noreply.github.com>
2024-11-13 11:55:04 -0500

    Update 2024.11.14 Watershed delineation.R

--- 2024.11.14 Watershed delineation.R
+++ 2024.11.14 Watershed delineation.R
@@ -43,16 +43,17 @@ if(!file.exists(file.path(proj_dir,"project_data.RData"))){
   source(file.path(proj_dir,"01_DataRetrieval_Cleaning_SaveLocal.R")) 
 } else {
   load(file.path(proj_dir, "project_data.RData")) # Load data layers/tables
+  us_dem = rast(dem_path) # read in the raster data, since it doesn't like loading from an .RData file
 }
 
 # Plot some of our cool new spatial layers!
 plot(conus$geometry) # the lower 48!
 plot(us_counties$geometry[us_counties$NAME_1=="Connecticut",]) # Hello CT
 plot(us_dem) # topography
-plot(us_states$geometry) # pop quiz, what is state super far out on the right? (answer at the bottom)
+plot(us_states$geometry) # Pop Quiz, what is the state super far out on the right? (answer at the bottom)
 
 
-# Pick our USGS gauge -----------------------------------------------------
+# Look at our USGS gauge -----------------------------------------------------
 
 w5_num = "01135300" #sleepers R
 
@@ -61,11 +62,10 @@ w5_num = "01135300" #sleepers R
 # First we will retrieve the lat and long info from USGS
 w5_info = readNWISdata(sites=w5_num, service="site")
 
+# Convert lat and long into a spatial object in R
 w5_spatial = st_as_sf(w5_info,
                       coords = c("dec_long_va","dec_lat_va"),
                       crs = st_crs(4326)) #assign WGS84 projection to coordinates
-w5_sp = st_transform(w5_spatial, st_crs(proj_proj)) # transform to the project projection
-
 
 # Calculate Drainage Basin ------------------------------------------------
 
@@ -74,7 +74,7 @@ basins_path = file.path(data_dir,"basin.shp")
 
 if(!file.exists(basins_path)){
   basin_info = drainageBasins(DEM=dem_path,
-                              points=w5_sp,
+                              points=w5_spatial,
                               points_name_col = "site_no",
                               save_path = data_dir)
   basins = st_as_sf(basin_info$delineated_basins)
@@ -83,9 +83,13 @@ if(!file.exists(basins_path)){
 }
  
 # What does our basin look like?
-plot(basins$geometry) # Pretty blocky! 
-plot(w5_sp$geometry, add=T, pch = 19, cex = 2, col = "darkcyan")
+plot(basins$geometry, col = "darkcyan") # Pretty blocky! Why is this?
+plot(w5_spatial$geometry, add=T, pch = 19, cex = 2, col = "darkblue")
 
+# What about some context?
+plot(us_counties$geometry[us_counties$NAME_1=="Vermont"])
+plot(basins$geometry, add=T, col = "darkcyan") 
+plot(w5_spatial$geometry, add=T, pch = 19, cex = 2, col = "darkblue")
 
 
 
@@ -111,7 +115,6 @@ w5_records$end_date
 # Also, for a general picture, we can look at the parameter groups.
 # https://help.waterdata.usgs.gov/codes-and-parameters/parameter-groups
 unique(w5_records$parm_grp_cd)
-
 
 
 
